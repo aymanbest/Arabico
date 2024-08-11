f78215e15690248664a8a1f7bb7b6d39bc92abfa
Ayman Faik <legmapapa@gmail.com>
2024-08-11 18:46:04 +0100

    added turrents to yaps

--- FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDProvider.kt
+++ FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDProvider.kt
@@ -12,7 +12,7 @@ import org.jsoup.nodes.Element
 
 class FaselHD : MainAPI() {
     override var lang = "ar"
-    override var mainUrl = "https://faselhd.express"
+    override var mainUrl = "https://www.faselhds.care"
     private  val alternativeUrl = "https://www.faselhd.club"
     override var name = "FaselHD"
     override val usesWebView = false
